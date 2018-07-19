c939c91076d7e328a5173f6714a2fa14f735aeff
Sirebellum <joshua.herrera227@gmail.com>
2018-07-19 11:36:49 -0500

    Filter out uncompleted jobs

--- swf.py
+++ swf.py
@@ -2,6 +2,24 @@ from __future__ import print_function
 
 import sys, os, time
 
+# dictionary that maps column name to integer index
+clmn  =  {"#": 0,
+          "submit": 1,
+          "wait": 2,
+          "run": 3,
+          "cpu": 4,
+          "time.cpu": 5, # per core
+          "mem": 6,
+          "cpu.req": 7,
+          "time.req": 8, # requested cpu time
+          "mem.req": 9,
+          "status": 10,
+          "usr": 11,
+          "grp": 12,
+          "exec": 13,
+          "queue": 14,
+          "partition": 15}
+
 # Parse a single line from swf file, without filtering.
 # Returns a list of all the elements in the line.
 # Returns next uncommented line (skips lines beginning with ;)
@@ -17,14 +35,20 @@ def parse_line(line):
 # Filter function for each line to be used in list comprehension.
 # Not specifying an option argument puts the function in "parse" mode
 # Specifying an option argument will set up filter options for the current session
-# Automatically filters out commented lines.
+# Automatically filters out commented lines and uncompleted jobs.
 def filter(line, options=None):
-
+    global clmn
+    
+    # Parse line for ease of filtering
+    string = parse_line(line)
+    
     # Filtering mode (no options for now)
     if options is None:
     
         if ";" in str(line):
             return False # Skip commented lines
+        if string[ clmn["status"] ] != "1":
+            return False
             
         # other filtering logic (to be implemented [tbi]) #
     
@@ -49,25 +73,6 @@ file = open(filename, 'rb')
 
 # parse swf file line by line
 dataset = [parse_line(line) for line in file if filter(line)]
-
-# dictionary that maps column name to integer index
-clmn  =  {"#": 0,
-          "submit": 1,
-          "wait": 2,
-          "run": 3,
-          "cpu": 4,
-          "time.cpu": 5, # per core
-          "mem": 6,
-          "cpu.req": 7,
-          "time.req": 8, # requested cpu time
-          "mem.req": 9,
-          "status": 10,
-          "usr": 11,
-          "grp": 12,
-          "exec": 13,
-          "queue": 14,
-          "partition": 15}
-
 
 # Cursory test of dataset/clmn structure
 #for job in dataset[0:10]:
