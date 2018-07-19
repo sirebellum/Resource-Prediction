8bb85bb5bbe021b5041b63a68a735b8864d02bd2
Sirebellum <joshua.herrera227@gmail.com>
2018-07-19 11:51:41 -0500

    Process dataset into data used for clustering

--- cluster.py
+++ cluster.py
@@ -10,7 +10,7 @@ clmn  =  {"#": 0,
           "run": 3,
           "cpu": 4,
           "time.cpu": 5, #per core
-          "mem": 6,
+          "mem": 6, #per core
           "cpu.req": 7,
           "time.req": 8,
           "mem.req": 9,
@@ -22,5 +22,21 @@ clmn  =  {"#": 0,
           "partition": 15}
 
 # Total memory usage per job (kb)
-mem = [int(job[ clmn["mem"] ])*int(job[ clmn["cpu" ] ]) for job in swf.dataset]
-print( "Max memory allocated for a single job: {} MB".format( max(mem)/1024 ) )
+mem_total = [int(job[ clmn["mem"] ])*int(job[ clmn["cpu" ] ]) \
+            for job in swf.dataset]
+print( "Max memory allocated for a single job: {} MB".format( max(mem_total)/1024 ) )
+
+swf.dataset = swf.dataset[0:1000] # for testing
+
+# Clustering data:
+# from Towards Autonomic Workload Provisioning for Enterprise Grids and Clouds, Quiroz et. al.
+mem = [int(job[ clmn["mem"] ]) \
+            for job in swf.dataset]
+cpu = [float(job[ clmn["cpu"] ])*float(job[ clmn["time.cpu"] ])/100.0 \
+             for job in swf.dataset]
+
+             
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    plt.plot(mem, cpu, 'ro')
+    plt.show()
