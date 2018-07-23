cc326613d9c1ed3636ceb002c859ddf211752620
Sirebellum <joshua.herrera227@gmail.com>
2018-07-23 16:51:04 -0500

    Reduced point count, helpful comments

--- predict.py
+++ predict.py
@@ -47,29 +47,36 @@ data = [ [cpureq[x], memreq[x], pindexreq[x] ] for x in range(len(pindexreq)) ]
 w = [predict( job[1], job[0], job[2] ) for job in data]
 
 if __name__ == "__main__":
-    # Plot
+
+    # Plot prediction surface
     fig = plt.figure(1)
     ax = fig.add_subplot(111, projection='3d')
 
+    # Generate axes
     x = np.arange(0, 3.5e4, 0.25e4) # mem
-    y = np.arange(0, 1100, 100)       # cpu
-    z = np.arange(0, 5e4, 0.5e4)  # pindex
+    y = np.arange(0, 1100, 100)     # cpu
+    z = np.arange(0, 5e4, 0.5e4)    # pindex
     
+    # Generate points and calculate points' values
     test = np.array(np.meshgrid(x, y, z)).T.reshape(-1,3)
-    #test = [ [x[i], y[i], z[i] ] for i in range(len(z)) ]
     c = np.array([predict( axes[0], axes[1], axes[2] ) for axes in test])
     
+    # Retrieve data points
     x = [ entry[0] for entry in test ]
     y = [ entry[1] for entry in test ]
     z = [ entry[2] for entry in test ]
     
+    # label axes
     ax.set_xlabel('Memory/VM')
     ax.set_ylabel('#VMs')
     ax.set_zlabel('pindex')
     
-    ax.invert_xaxis() # Match paper's orientation
+    # Match paper's orientation
+    ax.invert_xaxis()
+    # Plot real data over response surface
     ax.scatter(mem, cpu, pindex, c=pindexreq, cmap=plt.get_cmap('RdYlGn'), alpha=0.75)
     ax.scatter(x, y, z, c=c, cmap=plt.get_cmap('RdYlGn'), alpha=0.35)
+    
     
     # Compute accuracy
     ratios = [ safe_divide( w[i], pindexreq[i] ) for i in range(len(w)) ]
@@ -79,7 +86,7 @@ if __name__ == "__main__":
                          for i in range(0, len(ratios), 1000) ]
     
     print("Average ratio of predicted to expected response time: {}".format(average_ratio))
-    # Plot
+    # Plot average response time ratio
     fig2 = plt.figure(2)
     fig2.add_subplot(111)
     plt.plot(averages)
